6363865ca9d10aa6f4d91689002e0c6e4dc67607
tsyrelchukk <tsyrelchukk@gmail.com>
2026-07-09 17:38:54 +0300

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -17,7 +17,7 @@ my_data <- works |>
 # столбец с именем и фамилией на два новых: author, name. 
 # Удалите столбец name
 my_data2 <- my_data |>
-  separate(author, into = c("author", "name"), sep = " ", extra = "merge", fill = "right") |>  
+  separate_wider_delim(author, delim = " ", names = c("author", "name"), too_few = "align_start") |> 
   select(-name)
 
 
